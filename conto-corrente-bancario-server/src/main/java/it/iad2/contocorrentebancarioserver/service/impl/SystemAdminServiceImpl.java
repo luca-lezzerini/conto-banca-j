b229e75e918f66325fbc1c7129b3d6f9bbc25339
AulicinoADiego <aulicinodiego@gmail.com>
2021-03-20 17:20:28 +0100

    Risoluzione Parziale Ticket #6
    Associazioni Clienti con i vari Conti

--- conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/service/impl/SystemAdminServiceImpl.java
+++ conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/service/impl/SystemAdminServiceImpl.java
@@ -42,15 +42,15 @@ public class SystemAdminServiceImpl implements SystemAdminService {
     @Override
     public void generaDatiTest() {
         //Cancellare tutti i dati del DB
-        clienteRepository.deleteAllInBatch();
-        contoCorrenteRepository.deleteAllInBatch();
-        contoDepositoRepository.deleteAllInBatch();
-        contoPrestitoRepository.deleteAllInBatch();
         movCCRepository.deleteAllInBatch();
         movCDRepository.deleteAllInBatch();
         movCPRepository.deleteAllInBatch();
-
-        //Popolare Entità 
+        contoCorrenteRepository.deleteAllInBatch();
+        contoDepositoRepository.deleteAllInBatch();
+        contoPrestitoRepository.deleteAllInBatch();
+        clienteRepository.deleteAllInBatch();
+        
+        //Popola Tabella Cliente
         List<Cliente> clienti = new ArrayList<>();
         clienti.add(new Cliente("Maria", "Rossi", "MR12", "via 1", "4638292", LocalDate.now()));
         clienti.add(new Cliente("Alessio", "Verdi", "AV34", "via 2", "8594837", LocalDate.now()));
@@ -59,25 +59,25 @@ public class SystemAdminServiceImpl implements SystemAdminService {
         clienti.add(new Cliente("Chiara", "Viola", "CV76", "via 5", "73838372", LocalDate.now()));
         clienteRepository.saveAll(clienti);
         
-
+        //Popola Tabella Conto Corrente
         List<ContoCorrente> contiCorrenti = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 8; i++) {
             contiCorrenti.add(new ContoCorrente("CC" + i));
         }
-        contoCorrenteRepository.saveAll(contiCorrenti);
-
+        
+        //Popola Tabella Conto Deposito
         List<ContoDeposito> contiDepositi = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 5; i++) {
             contiDepositi.add(new ContoDeposito("CD" + i));
         }
-        contoDepositoRepository.saveAll(contiDepositi);
-
+        
+        //Popola Tabella Conto Prestito
         List<ContoPrestito> contiPrestiti = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 5; i++) {
             contiPrestiti.add(new ContoPrestito("CP" + i));
         }
-        contoPrestitoRepository.saveAll(contiPrestiti);
-
+        
+        //Popola Tabella Movimenti Conto Corrente 
         List<MovCC> movimentiCC = new ArrayList<>();
         movimentiCC.add(new MovCC(LocalDate.now(), "prelievo", 1000));
         movimentiCC.add(new MovCC(LocalDate.now(), "versamento", 200));
@@ -93,7 +93,8 @@ public class SystemAdminServiceImpl implements SystemAdminService {
         movimentiCC.add(new MovCC(LocalDate.now(), "riscatto", 550));
         movimentiCC.add(new MovCC(LocalDate.now(), "bonificoUscita", 700));
         movCCRepository.saveAll(movimentiCC);
-
+        
+        //Popola Tabella Movimenti Conto Deposito
         List<MovCD> movimentiCD = new ArrayList<>();
         movimentiCD.add(new MovCD(LocalDate.now(), "deposito", 100));
         movimentiCD.add(new MovCD(LocalDate.now(), "deposito", 300));
@@ -107,6 +108,7 @@ public class SystemAdminServiceImpl implements SystemAdminService {
         movimentiCD.add(new MovCD(LocalDate.now(), "riscatto", 650));
         movCDRepository.saveAll(movimentiCD);
         
+        //Popola Tabella Movimenti Conto Prestito
         List<MovCP> movimentiCP = new ArrayList<>();
         movimentiCP.add(new MovCP(LocalDate.now(), "concessione", 1000));
         movimentiCP.add(new MovCP(LocalDate.now(), "concessione", 500));
@@ -122,6 +124,52 @@ public class SystemAdminServiceImpl implements SystemAdminService {
         movimentiCP.add(new MovCP(LocalDate.now(), "erogazione", 300));
         movCPRepository.saveAll(movimentiCP);
         
-
+        //Associazioni Cliente-ContoCorrente
+        clienteRepository.getOne(clienti.get(0).getId()).getListaContiC().add(contiCorrenti.get(5));
+        contiCorrenti.get(5).setCliente(clienti.get(0));
+        clienteRepository.getOne(clienti.get(1).getId()).getListaContiC().add(contiCorrenti.get(7));
+        contiCorrenti.get(7).setCliente(clienti.get(1));
+        clienteRepository.getOne(clienti.get(2).getId()).getListaContiC().add(contiCorrenti.get(1));
+        contiCorrenti.get(1).setCliente(clienti.get(2));
+        clienteRepository.getOne(clienti.get(4).getId()).getListaContiC().add(contiCorrenti.get(6));
+        contiCorrenti.get(6).setCliente(clienti.get(4));
+        clienteRepository.getOne(clienti.get(0).getId()).getListaContiC().add(contiCorrenti.get(2));
+        contiCorrenti.get(2).setCliente(clienti.get(0));
+        clienteRepository.getOne(clienti.get(0).getId()).getListaContiC().add(contiCorrenti.get(0));
+        contiCorrenti.get(0).setCliente(clienti.get(0));
+        clienteRepository.getOne(clienti.get(2).getId()).getListaContiC().add(contiCorrenti.get(3));
+        contiCorrenti.get(3).setCliente(clienti.get(2));
+        clienteRepository.getOne(clienti.get(4).getId()).getListaContiC().add(contiCorrenti.get(4));
+        contiCorrenti.get(4).setCliente(clienti.get(4));
+        
+        //Associazioni Cliente-ContoDeposito
+        clienteRepository.getOne(clienti.get(1).getId()).getListaContiD().add(contiDepositi.get(3));
+        contiDepositi.get(3).setCliente(clienti.get(1));
+        clienteRepository.getOne(clienti.get(2).getId()).getListaContiD().add(contiDepositi.get(0));
+        contiDepositi.get(0).setCliente(clienti.get(2));
+        clienteRepository.getOne(clienti.get(3).getId()).getListaContiD().add(contiDepositi.get(1));
+        contiDepositi.get(1).setCliente(clienti.get(3));
+        clienteRepository.getOne(clienti.get(4).getId()).getListaContiD().add(contiDepositi.get(2));
+        contiDepositi.get(2).setCliente(clienti.get(4));
+        clienteRepository.getOne(clienti.get(1).getId()).getListaContiD().add(contiDepositi.get(4));
+        contiDepositi.get(4).setCliente(clienti.get(1));
+        
+        //Associazioni Cliente-ContoPrestito
+        clienteRepository.getOne(clienti.get(0).getId()).getListaContiP().add(contiPrestiti.get(3));
+        contiPrestiti.get(3).setCliente(clienti.get(0));
+        clienteRepository.getOne(clienti.get(2).getId()).getListaContiP().add(contiPrestiti.get(0));
+        contiPrestiti.get(0).setCliente(clienti.get(2));
+        clienteRepository.getOne(clienti.get(3).getId()).getListaContiP().add(contiPrestiti.get(1));
+        contiPrestiti.get(1).setCliente(clienti.get(3));
+        clienteRepository.getOne(clienti.get(3).getId()).getListaContiP().add(contiPrestiti.get(4));
+        contiPrestiti.get(4).setCliente(clienti.get(3));
+        clienteRepository.getOne(clienti.get(4).getId()).getListaContiP().add(contiPrestiti.get(2));
+        contiPrestiti.get(2).setCliente(clienti.get(4));
+        
+        // salva tutti i dati nel DB
+        contoCorrenteRepository.saveAll(contiCorrenti);
+        contoDepositoRepository.saveAll(contiDepositi);
+        contoPrestitoRepository.saveAll(contiPrestiti);
+        clienteRepository.saveAll(clienti);
     }
 }
