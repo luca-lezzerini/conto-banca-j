e17a655606f572d9df90124f89766065feb37e94
Angelita Doria <angiegiuse@gmail.com>
2021-03-20 11:44:29 +0100

    Risolto ticket #14
    modificato app-routing.module

--- conto-banca-j-client/client/src/app/app-routing.module.ts
+++ conto-banca-j-client/client/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GestioneClienteComponent } from './gestione-cliente/gestione-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"gestione-cliente", component: GestioneClienteComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- conto-banca-j-client/client/src/app/gestione-cliente/gestione-cliente.component.ts
+++ conto-banca-j-client/client/src/app/gestione-cliente/gestione-cliente.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestioneClienteComponent implements OnInit {
 
+  nome: string;
+  cognome: string;
+  codiceFiscale: string;
+  telefono: string;
+  indirizzo: string;
+
   constructor() { }
 
   ngOnInit(): void {
