6923992e4c10293488646040306d1118fe0446fd
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-03-20 17:24:06 +0100

    UC Gestione CP #9
    Creazione scheletro Dto, Service e Service Impl

--- conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/service/GestioneCPService.java
+++ conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/service/GestioneCPService.java
@@ -5,10 +5,18 @@
  */
 package it.iad2.contocorrentebancarioserver.service;
 
+import it.iad2.contocorrentebancarioserver.dto.ListaCPDto;
+
 /**
  *
  * @author Valerio
  */
 public interface GestioneCPService {
-    
+
+    ListaCPDto add(String codice);
+
+    ListaCPDto delete(String codice);
+
+    ListaCPDto edit(String codice);
+
 }
--- conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/service/impl/GestioneCPServiceImpl.java
+++ conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/service/impl/GestioneCPServiceImpl.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package it.iad2.contocorrentebancarioserver.service.impl;
 
-/**
- *
- * @author Valerio
- */
-public class GestioneCPServiceImpl {
+import it.iad2.contocorrentebancarioserver.dto.ListaCPDto;
+import it.iad2.contocorrentebancarioserver.service.GestioneCPService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GestioneCPServiceImpl implements GestioneCPService {
+
+    @Override
+    public ListaCPDto add(String codice) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListaCPDto delete(String codice) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListaCPDto edit(String codice) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
