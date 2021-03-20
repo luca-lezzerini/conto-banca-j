5c4edededfc679c81f051b7640f20558e1a899a5
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-03-20 18:16:57 +0100

    Client: richiesta approvazione modifica app-routing.module. gestione CP #19

--- conto-banca-j-client/client/src/app/app-routing.module.ts
+++ conto-banca-j-client/client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GestioneClienteComponent } from './gestione-cliente/gestione-cliente.component';
 import {GestioneCcComponent} from './gestione-cc/gestione-cc.component';
+import { GestioneCpComponent } from './gestione-cp/gestione-cp.component';
 
 const routes: Routes = [
   {path: 'gestione-cliente', component: GestioneClienteComponent},
-  {path: 'gestione-conto-corrente', component: GestioneCcComponent}
+  {path: 'gestione-conto-corrente', component: GestioneCcComponent},
+  {path: 'gestione-conto-prestito', component: GestioneCpComponent},
 ];
 
 @NgModule({
