5f2d11278c7e471b8d117d3674e61df6942d6bbd
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-03-21 16:53:50 +0100

    UC Mostra Tutti i Conti #11
    Creazione rotta nell'app routing

--- conto-banca-j-client/client/src/app/app-routing.module.ts
+++ conto-banca-j-client/client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { GestioneClienteComponent } from './gestione-cliente/gestione-cliente.component';
 import {GestioneCcComponent} from './gestione-cc/gestione-cc.component';
 import { GestioneCpComponent } from './gestione-cp/gestione-cp.component';
+import { MostraTuttiIContiComponent } from './mostra-tutti-i-conti/mostra-tutti-i-conti.component';
 
 const routes: Routes = [
   {path: 'gestione-cliente', component: GestioneClienteComponent},
   {path: 'gestione-conto-corrente', component: GestioneCcComponent},
   {path: 'gestione-conto-prestito', component: GestioneCpComponent},
+  {path: 'mostra-tutti-i-conti',component: MostraTuttiIContiComponent},
 ];
 
 @NgModule({
