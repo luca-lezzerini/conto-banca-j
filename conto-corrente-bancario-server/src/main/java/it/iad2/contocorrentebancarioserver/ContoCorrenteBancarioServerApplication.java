82c4550b6785689cb36953d3dac902bb264e2a2c
irenedg <79708656+irenedg@users.noreply.github.com>
2021-03-22 14:32:01 +0100

    Commit commento sopra il main

--- conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/ContoCorrenteBancarioServerApplication.java
+++ conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/ContoCorrenteBancarioServerApplication.java
@@ -2,16 +2,21 @@ package it.iad2.contocorrentebancarioserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Classe di avvio dell'applicazione
+ *
  * @author use
  */
 
 @SpringBootApplication
 public class ContoCorrenteBancarioServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ContoCorrenteBancarioServerApplication.class, args);
-	}
+    /**
+     *punto di partenza del programma
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ContoCorrenteBancarioServerApplication.class, args);
+    }
 
 }
