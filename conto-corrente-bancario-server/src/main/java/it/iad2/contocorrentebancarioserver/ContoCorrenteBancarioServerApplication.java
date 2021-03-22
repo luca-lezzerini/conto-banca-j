e5c4f310aa926edc49881aea0e1991ea041c1169
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-03-22 15:00:35 +0100

    Update ContoCorrenteBancarioServerApplication.java

--- conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/ContoCorrenteBancarioServerApplication.java
+++ conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/ContoCorrenteBancarioServerApplication.java
@@ -3,17 +3,9 @@ package it.iad2.contocorrentebancarioserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Classe di avvio dell'applicazione
- *
- * @author use
- */
 @SpringBootApplication
 public class ContoCorrenteBancarioServerApplication {
-    /**
-     * punto di inizio del programma
-     * @param args 
-     */
+
     public static void main(String[] args) {
         SpringApplication.run(ContoCorrenteBancarioServerApplication.class, args);
     }
