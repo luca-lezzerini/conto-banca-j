e1f43f26c8f5907f65759e9ed522c3d67f96d386
AulicinoADiego <aulicinodiego@gmail.com>
2021-03-22 14:15:12 +0100

    update ccb application

--- conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/ContoCorrenteBancarioServerApplication.java
+++ conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/ContoCorrenteBancarioServerApplication.java
@@ -2,7 +2,11 @@ package it.iad2.contocorrentebancarioserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author aulic
+ * Punto di partenza del programma
+ */
 @SpringBootApplication
 public class ContoCorrenteBancarioServerApplication {
 
