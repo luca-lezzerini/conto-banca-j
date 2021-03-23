45d834bae8c8b3a56ff1795d2b2805c82f94b79f
francescofilitti <79720164+francescofilitti@users.noreply.github.com>
2021-03-23 17:36:14 +0100

    #33
    Implementazione client

--- conto-banca-j-client/client/src/app/estratto-conto-cp/estratto-conto-cp.component.ts
+++ conto-banca-j-client/client/src/app/estratto-conto-cp/estratto-conto-cp.component.ts
@@ -1,4 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../gestione-cliente/cliente';
+import { ClienteDto } from '../gestione-cliente/cliente-dto';
+import { ListaClientiDto } from '../gestione-cliente/lista-clienti-dto';
+import { ContoPrestito } from '../gestione-cp/conto-prestito';
+import { FiltroCognomeDto } from '../mostra-tutti-i-conti/filtro-cognome-dto';
+import { ListaContiClienteDto } from '../mostra-tutti-i-conti/lista-conti-cliente-dto';
+import { ListaContoPrestitoDto } from './lista-conto-prestito-dto';
 
 @Component({
   selector: 'app-estratto-conto-cp',
@@ -7,9 +16,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstrattoContoCpComponent implements OnInit {
 
-  constructor() { }
+  cliente = new Cliente();
+  clienti: Cliente[] = [];
+  cognomeContains: string;
+
+
+  contoPrestito = new ContoPrestito();
+  contiPrestiti: ContoPrestito[] = [];
+
+
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  cerca() {
+    let dto: FiltroCognomeDto = new FiltroCognomeDto();
+    dto.cognome = this.cognomeContains;
+    let oss: Observable<ListaClientiDto> = this.http.post<ListaClientiDto>("http://localhost:8080/cerca-cliente", dto);
+    oss.subscribe(c => this.clienti = c.listaClienti);
+    this.cliente.cognome = "";
+  }
+
+  mostraConti(c: Cliente) {
+    let dto: ClienteDto = new ClienteDto();
+    dto.cliente = c;
+    let fx: Observable<ListaContoPrestitoDto> = this.http.post<ListaContoPrestitoDto>("http://localhost:8080/carica-conti", dto);
+    fx.subscribe(m => {
+      this.contiPrestiti = m.listaContiPrestito;
+
+    });
+
+  }
 }
+
+
+
---
+++ conto-banca-j-client/client/src/app/estratto-conto-cp/lista-conto-prestito-dto.ts
@@ -0,0 +1,5 @@
+import { ContoPrestito } from "../gestione-cp/conto-prestito";
+
+export class ListaContoPrestitoDto {
+    listaContiPrestito: ContoPrestito[];
+}
