09770aea50a650612265437c111f6b98ac9cc7c0
gianni.golia <gianni.golia@gmail.com>
2021-03-23 12:42:43 +0100

    Inizio Implementazione Ticket: #29

--- conto-banca-j-client/client/src/app/associa-cc/associa-cc.component.ts
+++ conto-banca-j-client/client/src/app/associa-cc/associa-cc.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-associa-cc',
   templateUrl: './associa-cc.component.html',
-  styleUrls: ['./associa-cc.component.css']
+  styleUrls: ['./associa-cc.component.css'],
 })
 export class AssociaCcComponent implements OnInit {
+  constructor() {}
+  cliente: Cliente = new Cliente();
+  listaClienti: Cliente[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
