a59a73167921405bd89c9c73756fb00123ef0f5f
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-03-24 17:16:11 +0100

    Update ContoCorrenteRepository.java

--- conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/repository/ContoCorrenteRepository.java
+++ conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/repository/ContoCorrenteRepository.java
@@ -4,11 +4,15 @@ import it.iad2.contocorrentebancarioserver.model.ContoCorrente;
 import java.util.List;
 import java.util.stream.Stream;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContoCorrenteRepository extends JpaRepository<ContoCorrente,Long>{
      List<ContoCorrente>findByCliente(Long i);
+     
+     // Query dummy per esempio in aula
+     @Query("select c from ContoCorrente c")
      Stream<ContoCorrente>findByCliente2(Long i);
      
      ContoCorrente findByNumConto(String numConto);
