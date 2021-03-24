e3e12520061bdf5534f6d1d531e4c84784d87274
Angelita Doria <angiegiuse@gmail.com>
2021-03-24 12:03:36 +0100

    Completato ticket #32

---
+++ conto-corrente-bancario-server/src/main/java/it/iad2/contocorrentebancarioserver/service/impl/EstrattoCCServiceImpl.java
@@ -0,0 +1,40 @@
+package it.iad2.contocorrentebancarioserver.service.impl;
+
+import it.iad2.contocorrentebancarioserver.dto.ListaCCDto;
+import it.iad2.contocorrentebancarioserver.dto.ListaMovCCDto;
+import it.iad2.contocorrentebancarioserver.model.Cliente;
+import it.iad2.contocorrentebancarioserver.model.ContoCorrente;
+import it.iad2.contocorrentebancarioserver.model.MovCC;
+import it.iad2.contocorrentebancarioserver.repository.ClienteRepository;
+import it.iad2.contocorrentebancarioserver.repository.ContoCorrenteRepository;
+import it.iad2.contocorrentebancarioserver.repository.MovCCRepository;
+import it.iad2.contocorrentebancarioserver.service.EstrattoCCService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstrattoCCServiceImpl implements EstrattoCCService{
+
+    @Autowired
+    ContoCorrenteRepository contoCCRepository;
+    @Autowired
+    MovCCRepository movCCRepository;
+    @Autowired
+    ClienteRepository clienteRepository;
+    
+    @Override
+    public ListaCCDto mostraContiCC(Cliente cliente) {
+        cliente = clienteRepository.findById(cliente.getId()).get();
+        List<ContoCorrente> listaCC = cliente.getListaContiC();
+        return new ListaCCDto(listaCC);
+    }
+
+    @Override
+    public ListaMovCCDto mostraMovCC(ContoCorrente contoCC) {
+        contoCC = contoCCRepository.findById(contoCC.getId()).get();
+        List<MovCC> movCC = contoCC.getListaMovCC();
+        return new ListaMovCCDto(movCC);
+    }
+
+}
