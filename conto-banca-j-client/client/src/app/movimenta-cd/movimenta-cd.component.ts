edbf74498c224eb7cb4991ae19224b993f44e47d
DESKTOP-J5C4BKM\Samuele <5>
2021-03-25 13:40:39 +0100

    html MovimentaCd #49

--- conto-banca-j-client/client/src/app/movimenta-cd/movimenta-cd.component.ts
+++ conto-banca-j-client/client/src/app/movimenta-cd/movimenta-cd.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MovCD } from '../estratto-conto-cd/mov-cd';
 
 @Component({
   selector: 'app-movimenta-cd',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movimenta-cd.component.css']
 })
 export class MovimentaCdComponent implements OnInit {
+  codice: string;
+  tipoMov: string;
+  importo: number;
+  listaMovCD: MovCD [] = [];
+  movCD: MovCD;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
   }
 
+  cerca(){
+    const dto: 
+  }
 }
