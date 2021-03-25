d7026b671f24a3ac9acd11e5ab09f6a1716131c0
francescofilitti <79720164+francescofilitti@users.noreply.github.com>
2021-03-25 01:14:53 +0100

    #48
    implementazione html movimenti cc

--- conto-banca-j-client/client/src/app/movimenta-cc/movimenta-cc.component.ts
+++ conto-banca-j-client/client/src/app/movimenta-cc/movimenta-cc.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { MovCC } from '../ec-cc/mov-cc';
 
 @Component({
   selector: 'app-movimenta-cc',
   templateUrl: './movimenta-cc.component.html',
-  styleUrls: ['./movimenta-cc.component.css']
+  styleUrls: ['../theme.css']
 })
 export class MovimentaCcComponent implements OnInit {
+  codice:string;
+  tipoMov:string="versamento";
+  importo:number;
+  listaMovCC:MovCC[];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+cerca(){}
+esegui(){}
 }
