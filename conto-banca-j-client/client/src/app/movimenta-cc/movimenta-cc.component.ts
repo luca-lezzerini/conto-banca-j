bf77bcfe85130261eea748bc445c94953de14ea5
danieleSantini <danielesantini94@gmail.com>
2021-03-25 18:01:15 +0100

    #48
    implementazione lato client

--- conto-banca-j-client/client/src/app/movimenta-cc/movimenta-cc.component.ts
+++ conto-banca-j-client/client/src/app/movimenta-cc/movimenta-cc.component.ts
@@ -1,5 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { MovCC } from '../ec-cc/mov-cc';
+import { ContoCorrente } from '../gestione-cc/contoCorrente';
+import { ListaCCDto } from '../gestione-cc/listaCCDto';
+import { numContoCCDto } from '../gestione-cc/numContoCCDto';
+import { MovimentoCCDto } from './mov-cc-dto';
+import { MovimentiCCDto } from './movimenti-cc-dto';
+import { MovimentoCC } from './movimento-cc';
 
 @Component({
   selector: 'app-movimenta-cc',
@@ -7,15 +15,31 @@ import { MovCC } from '../ec-cc/mov-cc';
   styleUrls: ['../theme.css']
 })
 export class MovimentaCcComponent implements OnInit {
-  codice:string;
-  tipoMov:string="versamento";
-  importo:number;
-  listaMovCC:MovCC[];
+  dataOdierna: Date;
+  contoCorrente: ContoCorrente = new ContoCorrente();
+  codice: string;
+  tipoMov: string = "versamento";
+  importo: number;
+  listaMovCC: MovimentoCC[];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit(): void {
   }
-cerca(){}
-esegui(){}
+
+  cerca() {
+    let dto: numContoCCDto = new numContoCCDto();
+    dto.numConto = this.contoCorrente.numConto;
+    let oss: Observable<numContoCCDto> = this.http.post<numContoCCDto>("http://localhost:8080/cerca-conto", dto);
+    oss.subscribe(c => this.codice = c.numConto);
+  }
+  esegui() {
+    let dto: MovimentoCCDto = new MovimentoCCDto();
+    dto.dataOdierna = this.dataOdierna;
+    dto.importo = this.importo;
+    dto.tipoMovimento = this.tipoMov;
+    let oss: Observable<MovimentiCCDto> = this.http.post<MovimentiCCDto>("http://localhost:8080/esegui-movimento", dto);
+    oss.subscribe(x => this.listaMovCC = x.listaMovimentiCC);
+  }
 }
